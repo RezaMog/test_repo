68a0f0bdae89617e10570566365ad1772a7cbba1
RezaMog <Reza.Moghaddaszadeh@philips.com>
2026-09-22 10:33:59 -0700

    add functin1

---
+++ function1.py
@@ -0,0 +1,12 @@
+def twosum(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+    
+    Returns:
+    int or float: The sum of the two numbers.
+    """
+    return a + b
